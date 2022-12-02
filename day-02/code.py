fe970071fe471763050bc68608a78a43e0ac264d
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-12-02 10:30:12 +0100

    Day 02: clean up

--- day-02/code.py
+++ day-02/code.py
@@ -15,14 +15,17 @@ class Moves(Enum):
     PAPER = 2
     SCISSORS = 3
 
-def round_outcome(own_move, opponent_move):
-    return (own_move - opponent_move + 1) % len(Moves)
-
+ALL_LEGIT_MOVE_VALUES = list(Moves)
 GAIN = 3
+
+def round_score(own_move, opponent_move):
+    round_outcome = (own_move.value - opponent_move.value + 1) % len(ALL_LEGIT_MOVE_VALUES)
+    return round_outcome * GAIN + own_move.value
+
 OPPONENT_DECRYPTION_MAP = {
-    "A": Moves.ROCK.value,
-    "B": Moves.PAPER.value,
-    "C": Moves.SCISSORS.value
+    "A": Moves.ROCK,
+    "B": Moves.PAPER,
+    "C": Moves.SCISSORS
 }
 
 OWN_DECRYPTION_MAP = {
@@ -36,13 +39,16 @@ total_score = 0
 for round in strategy:
     encrypted_opponent_move, encrypted_own_move = round.split(" ")
     opponent_move = OPPONENT_DECRYPTION_MAP[encrypted_opponent_move]
-    own_move = OWN_DECRYPTION_MAP[encrypted_own_move]
-    total_score += own_move + round_outcome(own_move, opponent_move) * GAIN
+    own_move = Moves(OWN_DECRYPTION_MAP[encrypted_own_move])
+    total_score += round_score(own_move, opponent_move)
 print(total_score)
 
 # PART 2
 def get_own_move(opponent_move, expected_outcome):
-    return (opponent_move + expected_outcome - 2) % len(Moves) + 1
+    idx_offset = expected_outcome - 2
+    opponent_move_idx = ALL_LEGIT_MOVE_VALUES.index(opponent_move)
+    own_move_idx = (opponent_move_idx + idx_offset) % len(ALL_LEGIT_MOVE_VALUES)
+    return ALL_LEGIT_MOVE_VALUES[own_move_idx]
 
 total_score = 0
 for round in strategy:
@@ -50,5 +56,5 @@ for round in strategy:
     opponent_move = OPPONENT_DECRYPTION_MAP[encrypted_opponent_move]
     expected_outcome = OWN_DECRYPTION_MAP[encrypted_round_outcome]
     own_move = get_own_move(opponent_move, expected_outcome)
-    total_score += own_move + round_outcome(own_move, opponent_move) * GAIN
+    total_score += round_score(own_move, opponent_move)
 print(total_score)

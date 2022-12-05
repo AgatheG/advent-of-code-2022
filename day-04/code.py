6c2d12e38888953c136634d2e3895f33da9ac423
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-12-05 09:42:05 +0100

    Smarter way for day 04 part 2

--- day-04/code.py
+++ day-04/code.py
@@ -23,14 +23,13 @@ for pair in pairs:
         nb_contained += 1
 print("Part 1", nb_contained)
 
-nb_duplicates = 0
+nb_duplicates = len(pairs)
 for pair in pairs:
     first_assignement, second_assignment = [p.split(DELIMITER) for p in pair]
     first_lower, first_upper, second_lower, second_upper = [
         int(first_assignement[0]), int(first_assignement[1]),
         int(second_assignment[0]), int(second_assignment[1])
     ]
-    if (first_lower <= second_lower <= first_upper or first_lower <= second_upper <= first_upper
-        or second_lower <= first_lower <= second_upper or second_lower <= first_upper <= second_upper):
-        nb_duplicates += 1
+    if first_upper < second_lower or second_upper < first_lower:
+        nb_duplicates -= 1
 print("Part 2", nb_duplicates)

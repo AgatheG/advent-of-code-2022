d897f8ea646ec8d487fdf7bc23b234eccb57a667
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-12-09 08:33:16 +0100

    Clean day 09

--- day-09/code.py
+++ day-09/code.py
@@ -28,42 +28,35 @@ def move_head(direction):
 
 def move_tail(distance):
     if abs(distance[0]) > 1 and abs(distance[1]) > 1:
-        coef_x, coef_y = -1*int(distance[0]/abs(distance[0])), -1*int(distance[1]/abs(distance[1]))
-        return (distance[0] + coef_x, distance[1] + coef_y)
-    if distance[0] < -1:
-        return (distance[0] + 1, distance[1])
-    if distance[0] > 1:
-        return (distance[0] - 1, distance[1])
-    if distance[1] > 1:
-        return (distance[0], distance[1] - 1)
-    if distance[1] < -1:
-        return (distance[0], distance[1] + 1)
-    return (0,0)
+        offset_x = -1 * int(distance[0] / abs(distance[0]))
+        offset_y = -1 * int(distance[1] / abs(distance[1]))
+        return distance + [offset_x, offset_y]
+    if abs(distance[0]) > 1:
+        offset_x = -1 * int(distance[0] / abs(distance[0]))
+        return [distance[0] + offset_x, distance[1]]
+    if abs(distance[1]) > 1:
+        offset_y = -1 * int(distance[1] / abs(distance[1]))
+        return [distance[0], distance[1] + offset_y]
+    return [0, 0]
+
+def get_nb_distinct_positions(nb_knots):
+    tail_distinct_positions = set()
+    knot_positions = [np.array([0,0]) for i in range(nb_knots)]
+    tail_distinct_positions = set()
+    for line in lines:
+        direction, steps = line.split(" ")
+        for i in range(int(steps)):
+            for knot_idx in range(nb_knots - 1):
+                h_position, t_position = knot_positions[knot_idx], knot_positions[knot_idx + 1]
+                if knot_idx == 0:
+                    h_position += move_head(direction)
+                t_position += move_tail(h_position - t_position)
+                if knot_idx + 1 == nb_knots - 1:
+                    tail_distinct_positions.add(str(t_position))
+    return len(tail_distinct_positions)
 
 # PART 1
-h_position = np.array([0,0])
-t_position = np.array([0,0])
-tail_distinct_positions = set()
-for line in lines:
-    direction, steps = line.split(" ")
-    for i in range(int(steps)):
-        h_position += move_head(direction)
-        t_position += move_tail(h_position - t_position)
-        tail_distinct_positions.add(str(t_position))
-print("Part 1: ", len(tail_distinct_positions))
+print("Part 1: ", get_nb_distinct_positions(2))
 
 # PART 2
-NB_KNOTS = 10
-knot_positions = [np.array([0,0]) for i in range(NB_KNOTS)]
-tail_distinct_positions = set()
-for line in lines:
-    direction, steps = line.split(" ")
-    for i in range(int(steps)):
-        for knot_idx in range(len(knot_positions) - 1):
-            h_position, t_position = knot_positions[knot_idx], knot_positions[knot_idx + 1]
-            if knot_idx == 0:
-                h_position += move_head(direction)
-            t_position += move_tail(h_position - t_position)
-            if knot_idx + 1 == len(knot_positions) - 1:
-                tail_distinct_positions.add(str(t_position))
-print("Part 2: ", len(tail_distinct_positions))
+print("Part 2: ", get_nb_distinct_positions(10))

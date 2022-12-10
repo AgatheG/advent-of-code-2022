e0e7bc261183f53286182d71757db7bdb2704864
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-12-10 18:56:19 +0100

    Cleaner day 10

--- day-10/code.py
+++ day-10/code.py
@@ -10,23 +10,23 @@ args = parser.parse_args()
 with open(args.file, "r") as file:
     lines = file.read().split('\n')
 
-# PART 1
-INTERESTING_CYCLES = [20] + [20 + 40*i for i in range(1, 6)]
+ADD_INSTRUCTION = "addx"
 
+# PART 1
 delta_x = 0
 register = 1
 total_signal_strength = 0
 instruction_idx = 0
 cycle = 1
 while instruction_idx < len(lines) - 1:
-    if cycle in INTERESTING_CYCLES:
+    if (cycle - 20) % 40 == 0:
         total_signal_strength += cycle * register
     if delta_x != 0:
         register += delta_x
         delta_x = 0
     else:
         line = lines[instruction_idx].split(" ")
-        if line[0] == "addx":
+        if line[0] == ADD_INSTRUCTION:
             delta_x = int(line[1])
         instruction_idx += 1
     cycle += 1
@@ -45,7 +45,7 @@ while instruction_idx < len(lines) - 1:
         delta_x = 0
     else:
         line = lines[instruction_idx].split(" ")
-        if line[0] == "addx":
+        if line[0] == ADD_INSTRUCTION:
             delta_x = int(line[1])
         instruction_idx += 1
     if cycle %40 == 0:

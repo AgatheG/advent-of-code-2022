2832175d4f45795a80ae16c9fb690c2c7d64bf80
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-12-10 06:54:37 +0100

    Day 10 cleaner

--- day-10/code.py
+++ day-10/code.py
@@ -10,51 +10,45 @@ args = parser.parse_args()
 with open(args.file, "r") as file:
     lines = file.read().split('\n')
 
-to_add = 0
-X = 1
-s = 0
-wait = False
-idx = 0
-cycle = 1
-L = [20, 60, 100, 140, 180, 220]
-while idx < len(lines) - 1:
-    if cycle in L:
-        s += cycle * X
-    if not wait:
-        line = lines[idx].split(" ")
-        if line[0] == "addx":
-            to_add = int(line[1])
-            wait = True
-        idx += 1
-    else:
-        X += to_add
-        wait = False
-    cycle += 1
-print(s)
+# PART 1
+INTERESTING_CYCLES = [20] + [20 + 40*i for i in range(1, 6)]
 
-to_add = 0
-s = ""
-X = 1
-wait = False
-idx = 0
+delta_x = 0
+register = 1
+total_signal_strength = 0
+instruction_idx = 0
 cycle = 1
-t = False
-L = [40 * (i+1) for i in range(5)]
-while idx < len(lines) - 1:
-    if X - 1 <= cycle%40 - 1 <= X + 1:
-        s += "#"
+while instruction_idx < len(lines) - 1:
+    if cycle in INTERESTING_CYCLES:
+        total_signal_strength += cycle * register
+    if delta_x != 0:
+        register += delta_x
+        delta_x = 0
     else:
-        s += " "
-    if not wait:
-        line = lines[idx].split(" ")
+        line = lines[instruction_idx].split(" ")
         if line[0] == "addx":
-            to_add = int(line[1])
-            wait = True
-        idx += 1
-    else:
-        X += to_add
-        wait = False
-    if cycle in L:
-        s += "\n"
+            delta_x = int(line[1])
+        instruction_idx += 1
     cycle += 1
-print(s)
+print("PART 1: ", total_signal_strength)
+
+# PART 2
+delta_x = 0
+CRT_image = ""
+register = 1
+instruction_idx = 0
+cycle = 1
+while instruction_idx < len(lines) - 1:
+    CRT_image += "#" if register - 1 <= cycle%40 - 1 <= register + 1 else " "
+    if delta_x != 0:
+        register += delta_x
+        delta_x = 0
+    else:
+        line = lines[instruction_idx].split(" ")
+        if line[0] == "addx":
+            delta_x = int(line[1])
+        instruction_idx += 1
+    if cycle %40 == 0:
+        CRT_image += "\n"
+    cycle += 1
+print("PART 2:\n" + CRT_image)
